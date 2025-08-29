39dc9653cb9869ca98c9474281804f2af93786d9
Navi97 <vaishnavipukale70@gmail.com>
2025-08-29 16:50:00 +0100

    Adding login fail test

--- src/test/java/OrangeProjectTest.java
+++ src/test/java/OrangeProjectTest.java
@@ -2,17 +2,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.time.Duration;
 
 
 public class OrangeProjectTest {
     private WebDriver webDriver;
-    @BeforeTest
+    @BeforeMethod
     public void before(){
         webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
@@ -31,10 +31,26 @@ public class OrangeProjectTest {
         loginButton.click();
         WebElement dashboard = webDriver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/header/div[1]/div[1]"));
         Assert.assertEquals(dashboard.getText(), "Dashboard");
+    }
+    @Test
+    public void loginFail(){
+        webDriver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        System.out.println(webDriver.getTitle());
+        WebElement username = webDriver.findElement(By.name("username"));
+        username.sendKeys("Heath");
+        WebElement password = webDriver.findElement(By.name("password"));
+        password.sendKeys("123");
+        WebElement loginButton = webDriver.findElement(By.xpath("//button[@type='submit']"));
+        loginButton.click();
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        WebElement invalidCredential = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                        By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/p")));
+        Assert.assertEquals(invalidCredential.getText(),"Invalid credentials");
 
     }
 
-    @AfterTest
+
+    @AfterMethod
     public void after(){
         webDriver.quit();
     }
